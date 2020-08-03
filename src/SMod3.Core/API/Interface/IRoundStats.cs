317d613722d744f39c5827411dd6bc6f1063f415
iRebbok <irebbok@outlook.com>
2020-08-03 19:22:54 +0300

    WIP: update IRoundStats

--- src/SMod3.Core/API/Interface/IRoundStats.cs
+++ src/SMod3.Core/API/Interface/IRoundStats.cs
@@ -2,22 +2,27 @@ namespace SMod3.API
 {
     public interface IRoundStats
     {
-        public abstract int NTFAlive { get; }
-        public abstract int ScientistsAlive { get; }
-        public abstract int ScientistsEscaped { get; set; }
-        public abstract int ScientistsDead { get; }
-        public abstract int ScientistsStart { get; }
-        public abstract int ClassDEscaped { get; set; }
-        public abstract int ClassDDead { get; }
-        public abstract int ClassDAlive { get; }
-        public abstract int ClassDStart { get; }
-        public abstract int Zombies { get; }
-        public abstract int SCPDead { get; }
-        public abstract int SCPKills { get; }
-        public abstract int SCPAlive { get; }
-        public abstract int SCPStart { get; }
-        public abstract int GrenadeKills { get; }
-        public abstract bool WarheadDetonated { get; }
-        public abstract int CiAlive { get; }
+        int NTFAlive { get; }
+        int CiAlive { get; }
+
+        int ScientistsAlive { get; }
+        int ScientistsEscaped { get; }
+        int ScientistsDead { get; }
+        int ScientistsStart { get; }
+
+        int ClassDEscaped { get; }
+        int ClassDDead { get; }
+        int ClassDAlive { get; }
+        int ClassDStart { get; }
+
+        int Zombies { get; }
+        int GrenadeKills { get; }
+
+        int SCPDead { get; }
+        int SCPKills { get; }
+        int SCPAlive { get; }
+        int SCPStart { get; }
+
+        bool WarheadDetonated { get; }
     }
 }
